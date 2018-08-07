429110e84ab57ffe76395e4513f379a39f06f1ff
Sofía Nuñez <31999837+sofiaanunez@users.noreply.github.com>
2018-08-06 21:44:44 -0300

    Update perfilControlador.php

--- app/Http/Controllers/perfilControlador.php
+++ app/Http/Controllers/perfilControlador.php
@@ -1,25 +1,11 @@
 <?php
-<<<<<<< HEAD
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
-=======
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-use App\User;
-
->>>>>>> e78e984497f270d19dd9b28924d7a86ac75c6098
 class perfilControlador extends Controller
 {
     public function perfil(){
       $datosPerfil = User::all();
-<<<<<<< HEAD
-=======
-
->>>>>>> e78e984497f270d19dd9b28924d7a86ac75c6098
       return view('perfil')->with('datosPerfil', $datosPerfil);
     }
 }
